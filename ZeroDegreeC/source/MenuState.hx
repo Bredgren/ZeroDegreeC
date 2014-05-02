935e4dbe13ec72f45d58334f576e318154bbaac1
Brandon Edgren <brandon.edgren@gmail.com>
2014-05-02 09:41:07 -0700

    Setup project.

---
+++ ZeroDegreeC/source/MenuState.hx
@@ -0,0 +1,37 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxMath;
+
+/**
+ * A FlxState which can be used for the game's menu.
+ */
+class MenuState extends FlxState {
+	/**
+	 * Function that is called up when to state is created to set it up.
+	 */
+	override public function create():Void {
+    var hello = new FlxText(10, 10, 0, "Hello");
+    add(hello);
+		super.create();
+	}
+
+	/**
+	 * Function that is called when this state is destroyed - you might want to
+	 * consider setting all objects this state uses to null to help garbage collection.
+	 */
+	override public function destroy():Void {
+		super.destroy();
+	}
+
+	/**
+	 * Function that is called once every frame.
+	 */
+	override public function update():Void {
+		super.update();
+	}
+}
